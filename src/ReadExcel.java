b8ac255025aa0ea4d93f50609b88b34ce5fcb009
bahma <bahma@Bahman-TP>
2018-02-17 18:04:46 +0100

    Added the ReadExcel file class to read input excel file using "Apach IO"

---
+++ src/ReadExcel.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import org.apache.poi.poifs.filesystem.*;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class ReadExcel {
+	
+	public void readExcel(String file) {
+		if (file == null) {
+			System.out.println("Look at Usage");
+			System.exit(0);
+		}
+		try {
+			POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+			HSSFSheet sheet = wb.getSheetAt(0);
+			HSSFRow row;
+			HSSFCell cell;
+
+			int rows; // No of rows
+			rows = sheet.getPhysicalNumberOfRows();
+
+			int cols = 0; // No of columns
+			int tmp = 0;
+
+			// This trick ensures that we get the data properly even if it doesn't start
+			// from first few rows
+			for (int i = 0; i < 10 || i < rows; i++) {
+				row = sheet.getRow(i);
+				if (row != null) {
+					tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+					if (tmp > cols)
+						cols = tmp;
+				}
+			}
+
+			for (int r = 0; r < rows; r++) {
+				row = sheet.getRow(r);
+				if (row != null) {
+					for (int c = 0; c < cols; c++) {
+						cell = row.getCell((short) c);
+						if (cell != null) {
+							// Your code here
+						}
+					}
+				}
+			}
+			wb.close();
+		} catch (IOException ioe) {
+			System.out.println("Error reading/writing faild");
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
