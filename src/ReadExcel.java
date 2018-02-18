6ce598aaf335904c57175d0980b01569a959296a
bahma <bahma@Bahman-TP>
2018-02-18 15:05:39 +0100

    read a smple data from excel file and print it out;

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -1,58 +1,23 @@
-import java.io.*;
-import org.apache.poi.poifs.filesystem.*;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class ReadExcel {
-	
-	public void readExcel(String file) {
-		if (file == null) {
-			System.out.println("Look at Usage");
-			System.exit(0);
-		}
-		try {
-			POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
-			HSSFWorkbook wb = new HSSFWorkbook(fs);
-			HSSFSheet sheet = wb.getSheetAt(0);
-			HSSFRow row;
-			HSSFCell cell;
+	public static void main(String[] args) {
+		Workbook wb = WorkbookFactory.create(new File("MyExcel.xls"));
+		Sheet s = wb.getSheetAt(0);
 
-			int rows; // No of rows
-			rows = sheet.getPhysicalNumberOfRows();
+		// Get the 11th row, creating if not there
+		Row r1 = s.getRow(10);
+		if (r1 == null) r1 = s.createRow(10);
 
-			int cols = 0; // No of columns
-			int tmp = 0;
+		// Get the 3rd column, creating if not there
+		Cell c2 = r1.getCell(2, Row.CREATE_NULL_AS_BLANK);
+		// Set a string to be the value
+		c2.setCellValue("Hello, I'm the cell C10!");
 
-			// This trick ensures that we get the data properly even if it doesn't start
-			// from first few rows
-			for (int i = 0; i < 10 || i < rows; i++) {
-				row = sheet.getRow(i);
-				if (row != null) {
-					tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-					if (tmp > cols)
-						cols = tmp;
-				}
-			}
-
-			for (int r = 0; r < rows; r++) {
-				row = sheet.getRow(r);
-				if (row != null) {
-					for (int c = 0; c < cols; c++) {
-						cell = row.getCell((short) c);
-						if (cell != null) {
-							// Your code here
-						}
-					}
-				}
-			}
-			wb.close();
-		} catch (IOException ioe) {
-			System.out.println("Error reading/writing faild");
-		}catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
+		// Save
+		FileOutputStream out = new FileOutputStream("New.xls");
+		wb.write(out);
+		out.close();
 	}
 }
