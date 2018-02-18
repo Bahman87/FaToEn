185e4c611dba7eea964ae92d6d61f00672e343d2
bahma <bahma@Bahman-TP>
2018-02-18 15:08:45 +0100

    read a cell information from a sample excel

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -1,23 +1,20 @@
-import org.apache.poi.sl.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcel {
-	public static void main(String[] args) {
-		Workbook wb = WorkbookFactory.create(new File("MyExcel.xls"));
-		Sheet s = wb.getSheetAt(0);
 
-		// Get the 11th row, creating if not there
-		Row r1 = s.getRow(10);
-		if (r1 == null) r1 = s.createRow(10);
-
-		// Get the 3rd column, creating if not there
-		Cell c2 = r1.getCell(2, Row.CREATE_NULL_AS_BLANK);
-		// Set a string to be the value
-		c2.setCellValue("Hello, I'm the cell C10!");
-
-		// Save
-		FileOutputStream out = new FileOutputStream("New.xls");
-		wb.write(out);
-		out.close();
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		File src= new File("C:\\Users\\bahma\\ExcelData\\TestData.xlsx");
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook wb =new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		String data0 = sheet.getRow(0).getCell(0).getStringCellValue();
+		System.out.println(data0);
+		wb.close();
 	}
+
 }
