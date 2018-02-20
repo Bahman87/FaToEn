64b0efa2072ca742c651983b078435c2f20a1cc4
bahma <bahma@Bahman-TP>
2018-02-20 01:16:55 +0100

    Read and write is almost ready , next step to develop a converting Process

--- src/ConvertFarsiToEnglish.java
+++ src/ConvertFarsiToEnglish.java
@@ -1,20 +1,44 @@
+import java.io.*;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ConvertFarsiToEnglish {
 
 	public static void main(String[] args) {
 
+		if (args.length < 1) {
+			System.out.println("Usage: C:\\Program Folder>java -jar convertor.jar <myfile.xlsx> <column number> ");
+			System.exit(0);
+		}
+
 		try {
-			ReadExcel reader = new ReadExcel(0, "C:\\Users\\bahma\\ExcelData\\TestData.xlsx");
-			for (int i = 0; i < 5; i++) {
-				for (int j = 0; j < 2; j++) {
+
+		
+			String path = args[0];
+			int columnCount = Integer.parseInt(args[1]);
+			File file = new File(path);
+			FileInputStream fis = new FileInputStream(file);
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			GetExcelSize rowNum = new GetExcelSize(0, file);
+			ReadExcel reader = new ReadExcel(0, file);
+			XSSFSheet sheet = wb.getSheetAt(1);
+			for (int i = 0; i < rowNum.getSize() + 1; i++) {
+				sheet.createRow(i);
+				for (int j = 0; j < columnCount; j++) {
 					String str = reader.getData(i, j);
-					System.out.println(str);
-				}
+					sheet.getRow(i).createCell(j).setCellValue(str);
+				}	
 			}
+			FileOutputStream fos = new FileOutputStream(file);
+			wb.write(fos);
+			wb.close();
+			System.exit(0);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 
 }
--- src/GetExcelSize.java
+++ src/GetExcelSize.java
@@ -4,22 +4,21 @@ import java.io.FileInputStream;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ReadExcel {
+public class GetExcelSize {
 	
 	private FileInputStream fis;
 	private XSSFWorkbook wb;
 	private XSSFSheet sheet;
 
-	public ReadExcel(int exlSheet, File src) throws Exception {
+	public GetExcelSize(int exlSheet,File src) throws Exception {
+
 		fis = new FileInputStream(src);
 		wb = new XSSFWorkbook(fis);
 		sheet = wb.getSheetAt(exlSheet);
-
 	}
-
-	public String getData(int row, int column) throws Exception {
-		String data = sheet.getRow(row).getCell(column).getStringCellValue();
-		return data;
+	
+	public int getSize() {
+		int size =  sheet.getLastRowNum();
+		return size;
 	}
-
 }
